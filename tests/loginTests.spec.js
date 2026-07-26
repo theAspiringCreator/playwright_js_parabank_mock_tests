efff3ea05ce8335b2bd19b9c52e4aa33284fc0f1
theAspiringCreator <jaroslav.rynik@gmail.com>
2026-07-26 17:03:51 +0200

    adding new assertion and waits to login tests

--- tests/loginTests.spec.js
+++ tests/loginTests.spec.js
@@ -2,19 +2,13 @@ import { expect } from '@playwright/test';
 import { test} from './fixtures.spec.js';
 import testData from '../test-data/login-test-data.json' with { type: 'json' };
 
+test('Logging in with the correct data',async ({fixVisitBaseUrl, page} ) => {
+       const { hp, rp, acc } = fixVisitBaseUrl;
+       const d = testData.correctData;
 
-  test('Logging in with the correct data',async ({fixVisitBaseUrl} ) => {
-  const { hp, rp, acc } = fixVisitBaseUrl;
-
-
-   //variable to get login data from json file
-  const d = testData.correctData;
-
-  //test steps  
-     await hp.initializeDb();
-     await hp.goToRegistration();
-   
-     await rp.performReg(
+       await hp.initializeDb();
+       await hp.goToRegistration();
+       await rp.performReg(
                      d.dataFirstName,
                      d.dataLastName,
                      d.dataStreet,
@@ -28,23 +22,19 @@ import testData from '../test-data/login-test-data.json' with { type: 'json' };
                      d.dataConfPassword
                      );
 
-      /**
+                    /**
        * after sucessful registration the app logs the user in automatically
        * to test login feature one has to perform logout first
        */
-     await hp.performLogout();
+     await hp.performLogout();    
+     await test.step('Přihlášení uživatele', async () => {
+  await hp.performLogin(txtUsername, txtPassword);
+}, { timeout: 5000 });
 
-     await hp.performLogin(d.dataUsername, d.dataPassword);
-
-     //no error msg appears
      await expect(hp.getErrorMsgTitle()).not.toBeVisible();  
      await expect(hp.getErrorMsgText()).not.toBeVisible();
-
-     //welcome message in left panel appears 
      await expect(acc.getTextWelcomeTitlLeft()).toBeVisible();
-
-     //logout after performing validations
-     await hp.performLogout();
+     await hp.performLogout();   
 });
 
 testData.incorrectData.forEach((scenario) => {
@@ -61,7 +51,8 @@ testData.incorrectData.forEach((scenario) => {
      //test steps
      await hp.initializeDb();
      await hp.goToRegistration();
-
+     await page.waitForURL('https://parabank.parasoft.com/parabank/register.htm');
+  
      await rp.performReg(
                      d.dataFirstName,
                      d.dataLastName,
@@ -76,22 +67,23 @@ testData.incorrectData.forEach((scenario) => {
                      d.dataPassword_reg
                      );
 
-
+         
        
-       /**
-       * after sucessful registration the app logs the user in automatically
-       * to test login feature one has to perform logout first
-       */
-
+       //
+       // after sucessful registration the app logs the user in automatically
+       //to test login feature one has to perform logout first
+       
+       await page.waitForURL('https://parabank.parasoft.com/parabank/register.htm');
        await hp.performLogout();
-    
+       await page.goto('https://parabank.parasoft.com/parabank/index.htm?ConnType=JDBC');
+       await hp.performLogin(txtUsername, txtPassword);       
+       await page.waitForURL('https://parabank.parasoft.com/parabank/login.htm'); 
 
-       await hp.performLogin(txtUsername, txtPassword);
-
-       //error msg appears
        await expect(hp.getErrorMsgTitle()).toBeVisible();  
        await expect(hp.getErrorMsgText()).toBeVisible(); 
        await expect(hp.getErrorMsgTitle()).toContainText('Error!');
        await expect(hp.getErrorMsgText()).toContainText(errorText);
+       await expect(hp.getErrorMsgTitle()).toHaveCount(1);
+       await expect(hp.getErrorMsgText()).toHaveCount(1);
 });
 });
